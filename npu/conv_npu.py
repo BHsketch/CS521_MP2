7f9c749caccd1d39cc2ce272e816b938d7546236
bhavya <bhavya25hirani@gmail.com>
2025-10-05 16:18:23 -0500

    Version 3 works with bias, and passes first perf test!

--- npu/conv_npu.py
+++ npu/conv_npu.py
@@ -148,7 +148,6 @@ def conv2d(X, W, bias):
 
                     for filter_i in nl.affine_range(filter_height):
                         for filter_j in nl.affine_range(filter_width):
-                            # shift_ij = (filter_i*input_width + filter_j)
                             res_psum += nl.matmul(weights_slice[:, (c_in_pmax*i):(c_in_pmax*(i+1)), filter_i, filter_j], image_tile[:, (filter_i):(filter_i + tile_size_rows), (filter_j):(filter_j + out_width)])
                 
                 res_psum = nisa.tensor_scalar(res_psum, np.add, bias_vec[o])
