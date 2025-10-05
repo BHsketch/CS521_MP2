c61c325d49011f843e0dcd2f1666a4eb1a71a7df
bhavya <bhavya25hirani@gmail.com>
2025-10-05 01:02:11 -0500

    minor change

--- npu/conv_npu.py
+++ npu/conv_npu.py
@@ -135,7 +135,7 @@ def conv2d(X, W, bias):
                             shift_ij = (filter_i*input_width + filter_j)
                             res_psum += nl.matmul(weights_slice[:, (c_in_pmax*i):(c_in_pmax(i+1)), (i*filter_width + j)], image_tile[:, shift_ij:(tile_size_pixels + shift_ij)])
                 
-                 nl.store(X_out_re[b, (c_out_pmax*o):(c_out_pmax*(o+1)), (tile_size_pixels*p):(tile_size_pixels*(p+1))], value=res_psum)
+                nl.store(X_out_re[b, (c_out_pmax*o):(c_out_pmax*(o+1)), (tile_size_pixels*p):(tile_size_pixels*(p+1))], value=res_psum)
 
     X_out = X_out_re.reshape((batch_size, out_channels, out_pool_height, out_pool_width))
 
