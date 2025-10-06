4d5e5cce38214ae26f8d409f228e7875f64beee4
bhavya <bhavya25hirani@gmail.com>
2025-10-06 11:16:57 -0500

    pytorch implementation works on cpu

--- gpu/myconv.py
+++ gpu/myconv.py
@@ -2,8 +2,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.profiler import profile, record_function, ProfilerActivity
+import math
 
 device = torch.device("cuda:0")
+# device = torch.device("cpu")
 
 class ConvModel(nn.Module):
     def __init__(self, H, W, in_channels=3, out_channels=8, kernel_size=3, stride=1, padding=1):
@@ -21,8 +23,8 @@ class ConvModel(nn.Module):
         # TO DO: Define static shapes here. 
 
         # Precompute output size
-        self.out_h = (H - kernel_size + 1) // stride
-        self.out_w = (W - kernel_size + 1) // stride
+        self.out_h = (H + 2*padding - kernel_size) // stride + 1
+        self.out_w = (W + 2*padding - kernel_size) // stride + 1
 
         self.weight = nn.Parameter(torch.randn((out_channels, in_channels, kernel_size, kernel_size), device=device))
         self.bias = nn.Parameter(torch.zeros((out_channels), device=device))
@@ -55,15 +57,16 @@ class ConvModel(nn.Module):
 
                     # X shape is (N, C, H, W)
                     # extracting a chunk that when multiplied with the kernel, results in a single output element
-                    input_subtensor = x[n, :, (input_start_i):(input_start_i+KH), (input_start_j):(input_start_j+KW)]
+                    input_subtensor = x_pad[n, :, (input_start_i):(input_start_i+KH), (input_start_j):(input_start_j+KW)]
 
                     # convert 3d subtensor into a single row
-                    input_subtensor_flattened = input_subtensor.reshape((C*KH*KW, 1))
+                    input_subtensor_flattened = input_subtensor.reshape(-1) 
 
                     # Each block from the input should form one column in the output
-                    patches[n, (output_i*out_w + output_j),:] = input_subtensor_flattened.permute(1, 0) 
+                    patches[n, (output_i*out_w + output_j),:] = input_subtensor_flattened
 
 
+        print("patches: \n", patches, "\n")
         return patches
 
     def conv2d_manual(self, x):
@@ -72,44 +75,52 @@ class ConvModel(nn.Module):
         KH = KW = self.kernel_size
 
         # TO DO: 1) convert input (x) into shape (N, out_h*out_w, C*KH*KW).
-        cols = self.im2col_manual(x)          
+        cols = self.im2col_manual(x)
+        cols_t = torch.transpose(cols, 1, 2)
+        print("cols_t: \n", cols_t, "\n")
 
         # TO DO: 2) flatten self.weight into shape (C_out, C*KH*KW).
-        weight_flattened = self.weight.reshape((C_out, C*KH*KW))
+        weights_flattened = self.weight.reshape((C_out, (self.in_channels)*KH*KW))
+        print("weights_flattened:\n", weights_flattened, "\n")
 
         # TO DO: 3) perform tiled matmul after required reshaping is done.
-        tile_size_i = 32
-        tile_size_j = 32
-        tile_size_k = 32
+        tile_size_i = 15
+        tile_size_j = 15
+        tile_size_k = 15
         max_i = C_out 
         max_j = (self.out_h*self.out_w)
         max_k = (self.in_channels*self.kernel_size*self.kernel_size)
-        num_i_tiles = max_i // tile_size_i
-        num_j_tiles = max_j // tile_size_j
-        num_k_tiles = max_k // tile_size_k
+        num_i_tiles = math.ceil(max_i / tile_size_i)
+        num_j_tiles = math.ceil(max_j / tile_size_j)
+        num_k_tiles = math.ceil(max_k / tile_size_k)
         
         output = torch.zeros((N, C_out,(self.out_h*self.out_w)), device=device)
-        for n in range(N):
-            for ii in range(num_i_tiles):
-                for jj in range(num_j_tiles):
-                    for kk in range(num_k_tiles):
-                        limit_i = min(tile_size_i*(ii+1), max_i)
-                        limit_j = min(tile_size_j*(jj+1), max_j)
-                        limit_k = min(tile_size_k*(kk+1), max_k)
-                        output[n, :,:]torch.matmul(weights_flattened[(tile_size_i*ii):(limit_i), (tile_size_k*kk):(limit_k)], cols[n,(tile_size_k*kk):(limit_k), (tile_size_j*jj):(limit_j)])
-                        # for i in range(limit_i):
-                            # for j in range(limit_j):
-                                # for k in range(limit_k):
-                                    # output[n, i, j] += weight_flattened[i, k] * cols[n, k, j]
+
+        for ii in range(num_i_tiles):
+            for jj in range(num_j_tiles):
+                for kk in range(num_k_tiles):
+                    limit_i = min(tile_size_i*(ii+1), max_i)
+                    limit_j = min(tile_size_j*(jj+1), max_j)
+                    limit_k = min(tile_size_k*(kk+1), max_k)
+                    if(ii == 0 and jj == 0):
+                        print("first weights tile: ", weights_flattened[(tile_size_i*ii):(limit_i), :])
+                        print("first output tile: ", cols_t[:,:, (tile_size_j*jj):(limit_j)])
+                    output[:, (tile_size_i*ii):(limit_i), (tile_size_j*jj):(limit_j)] += torch.matmul(weights_flattened[(tile_size_i*ii):(limit_i), (tile_size_k*kk):(limit_k)], cols_t[:,(tile_size_k*kk):(limit_k), (tile_size_j*jj):(limit_j)])
+                    # output[n, :,:] = torch.matmul(weights_flattened[(tile_size_i*ii):(limit_i), (tile_size_k*kk):(limit_k)], cols[n,(tile_size_k*kk):(limit_k), (tile_size_j*jj):(limit_j)])
+                    # for i in range(limit_i):
+                        # for j in range(limit_j):
+                            # for k in range(limit_k):
+                                # output[n, i, j] += weight_flattened[i, k] * cols[n, k, j]
 
 
         # TO DO: 4) Add bias.
-        bias_col = self.bias.reshape((out_channels, 1))
+        bias_col = self.bias.reshape((C_out, 1))
         final_out = output + bias_col
 
         # TO DO: 5) reshape output into shape (N, C_out, out_h, out_w).
-        final_out = final_out.reshape((N, C_out, out_h, out_w)) 
+        final_out = final_out.reshape((N, C_out, self.out_h, self.out_w)) 
 
+        print("final output: \n", final_out, "\n")
         return final_out
         #return out
 
@@ -120,6 +131,7 @@ if __name__ == "__main__":
     torch.manual_seed(0)
     N, C, H, W = 2, 4, 22, 22
     x = torch.randn(N, C, H, W, device=device) 
+    print("X: \n", x, "\n")
     out_channels=8
     kernel_size=7
     model = ConvModel(H, W, C, out_channels, kernel_size, stride=1, padding=1).to(device)
@@ -127,5 +139,6 @@ if __name__ == "__main__":
 
     # Test your solution
     conv_ref = F.conv2d(x, model.weight, model.bias, stride=1, padding=1)
+    print("reference output: \n", conv_ref)
     print("PyTorch --- shape check:", out.shape == conv_ref.shape)
     print("PyTorch --- correctness check:", torch.allclose(out, conv_ref, atol=1e-4))
